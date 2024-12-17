a7cb454c6e7d5cd5a3d48c27673dfea9a00af15d
Quasar Jarosz <quasar@uams.edu>
2024-12-17 12:59:36 -0600

    Another attempt at Day 16, another failure

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,85 +1,129 @@
 mod util;
-// use std::collections::HashSet;
+use std::collections::HashSet;
+use std::cmp::Ordering;
 use util::{Board, Direction, Point};
 
 type Maze = Board;
 
+impl Maze {
+    fn get_start(&self) -> Point {
+        let start = self
+            .all_points()
+            .into_iter()
+            .filter(|x| self.get(x) == Some('S'))
+            .next() // take only the first thing matched
+            .expect("maze has no start?"); 
+        start
+    }
+    fn get_end(&self) -> Point {
+        let start = self
+            .all_points()
+            .into_iter()
+            .filter(|x| self.get(x) == Some('E'))
+            .next() // take only the first thing matched
+            .expect("maze has no end?"); 
+        start
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Path {
+    steps: HashSet<Point>,
+    last_step: Point,
+}
+impl Path {
+    fn from_other(other: &Path, step: &Point) -> Self {
+        let mut new_steps: HashSet<Point> = HashSet::new();
+        new_steps.extend(&other.steps);
+        new_steps.insert(*step);
+
+        Path {
+            steps: new_steps,
+            last_step: *step
+        }
+    }
+    fn empty(step: &Point) -> Self {
+        let mut new_steps: HashSet<Point> = HashSet::new();
+        new_steps.insert(*step);
+
+        Path {
+            steps: new_steps,
+            last_step: *step
+        }
+    }
+    fn get_next_steps(&self, maze: &Maze) -> Vec<Point> {
+        let adjacent_steps: Vec<_> = Direction::all()
+            .iter()
+            .map(|d| d.step(&self.last_step))
+            .collect();
+
+        let available_steps: Vec<_> = adjacent_steps
+            .into_iter()
+            // exclude walls
+            .filter(|p| maze.get(p) != Some('#'))
+            // exclude if we've visited it already on this path
+            .filter(|p| !self.steps.contains(p))
+            .collect();
+
+        available_steps
+    }
+}
+
+// paths are ordered by their length
+impl Ord for Path {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.steps.len().cmp(&other.steps.len())
+    }
+}
+
+impl PartialOrd for Path {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
 fn main() {
     println!("AoC 2024: Day 16");
 
-    let lines = util::get_lines_from_file("input/day16-test.txt");
-    // let lines = util::get_lines_from_file("input/day16.txt");
 
+    // let lines = util::get_lines_from_file("input/day16-test.txt");
+    let lines = util::get_lines_from_file("input/day16.txt");
 
 
     let maze = Maze { lines };
-    dbg!(&maze);
 
+    // // always start facing East, according to instructions
+    // let start_direction = Direction::East;
 
-    // idea 1:
-    // recursive function "visit"
-    // has to take input of: the direction we're going,
-    //                       a vec of nodes we have visited on this path,
-    //                       the node we are looking at now
-    //
-    // if the node/position is a wall, return None (this is a dead end)
-    // if the node is E, return ??? (maybe 1?)
-    // otherwise keep searching:
-    //     add ourself to the end of the visited_nodes vec (new copy)
-    //     collect:
-    //          visit in the current direction + 1
-    //          visit in each other direction + 1000 (turning penalty)
-    //          except: if any of those directions are in the
-    //                  list of nodes we have visited, don't visit it
-    //
-    //      if all come back as None, return None
-    //      If any come back with a score, return the lowest score
-
-    let start = maze
-        .all_points()
-        .into_iter()
-        .filter(|x| maze.get(x) == Some('S'))
-        .next() // take only the first thing matched
-        .expect("maze has no start?"); 
-
-    // always start facing East, according to instructions
-    let start_direction = Direction::East;
-
-    // initial call: TODO find the Start point first (hardcoded here)
-    let r = visit(&maze, &start_direction, &Vec::new(), start);
-    dbg!(r);
-
-}
-
-fn visit(maze: &Maze, heading: &Direction, visited: &Vec<Point>, pos: Point) -> Option<i32> {
-    if let Some(val) = maze.get(&pos) {
-        if val == '#' {
-            return None; // dead-end reached
-        }
-        if val == 'E' { // found the end!
-            return Some(0); 
-        }
-        let mut visited_ext = visited.clone();
-        visited_ext.push(pos);
-        let mut legs: Vec<i32> = Vec::new();
-
-        for d in Direction::all() {
-            let cost = if &d == heading { 1 } else { 1001 };
-            let new_pos = d.step(&pos);
-            if !visited_ext.contains(&new_pos) {
-                if let Some(c) = visit(maze, &d, &visited_ext, new_pos) {
-                    legs.push(c + cost);
-                }
-            }
-        }
-        // return the least costly leg
-        let cheapest_leg = legs
-            .iter()
-            .min()
-            .copied();
-        dbg!(&cheapest_leg);
-        cheapest_leg
-    } else {
-        None
+    if let Some(solution) = solve(&maze) {
+        println!("Solution found! {} steps", solution.steps.len());
     }
+}
+fn solve(maze: &Maze) -> Option<Path> {
+    let start_pos = maze.get_start();
+    let end_pos = maze.get_end();
+
+    let mut candidates: Vec<Path> = Vec::new();
+    candidates.push(Path::empty(&start_pos));
+    loop {
+        if candidates.len() == 0 {
+            break;
+        }
+        candidates.sort();
+        // easier to reverse than to pop off the top
+        candidates.reverse();
+        let candidate = candidates.pop().unwrap();
+
+        for step in candidate.get_next_steps(&maze) {
+            let new_path = Path::from_other(&candidate, &step);
+            if step == end_pos {
+                return Some(new_path);
+            } else {
+                candidates.push(new_path);
+            }
+
+        }
+    }
+
+    None
 }
